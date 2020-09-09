efea931e04708ff77ad39ed8e0514c37501d0cc3
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-09-09 17:48:29 +0530

    adding quick responce feature

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ let count = 0;
 document.addEventListener("keydown", (event) => {
   if (event.key === "Shift" || event.key === "Enter") {
     count++;
-    if (count === 1) {
-      setTimeout(() => {
-        count = 0;
-      }, 3000);
-    }
-    if (count === 5) {
-      console.log("Sticky Key");
-      count = 0;
-    }
+  }
+  if (count >= 5) {
+    console.log("sticky key");
+    count = 0;
   }
 });
+
+setInterval(() => {
+  count = 0;
+}, 3000);
